9960a66915527931a7dc78d564fb488ea2035a27
Kimio Kuramitsu <kkuramitsu@gmail.com>
2022-11-28 19:34:04 +0900

    streamlit_key

---
+++ frontend/build/precache-manifest.aa593a7db68710b4a563f57ff08c6ccc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5d7cd07c67f6dbae88952073440b19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e58ce0946d19b1982401",
+    "url": "./static/js/2.7aaaeacc.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.7aaaeacc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272b1f1dfdad9acae09c",
+    "url": "./static/js/main.96cd2776.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
